3a21d0c9b69645fb95db2e22da84dee9b6ccf2d2
Jace-Yang <420478365@qq.com>
2020-12-24 22:10:01 +0800

    V1.0.1 Initial Commit

---
+++ source/memory.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pandas as pd
+
+# 报告内存
+def sizeof_fmt(num, suffix='B'):
+    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
+
+# 返回内存使用情况
+def get_memory_usage():
+    return np.round(psutil.Process(os.getpid()).memory_info()[0]/2.**30, 2) 
+
+# 减少内存使用
+def reduce_mem_usage(df, verbose=True):
+    # df: pandas dataframe to reduce size             
+    # verbose: whether output
+    
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    start_mem = df.memory_usage().sum() / 1024**2    
+    for col in df.columns:
+        col_type = df[col].dtypes
+        if col_type in numerics:
+            c_min = df[col].min() # Get the minimum data type in whole pandas dataframe
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                        # np.iinfo(np.int8).min: 表示int8的下限 -128
+                        # 用最大最小值 确定该列可以被int8包含，因此将数据转换为int8
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)  
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)    
+    end_mem = df.memory_usage().sum() / 1024**2
+    if verbose: 
+        print('Memory usage decreased to {:5.2f} Mb now, with ({:.1f}% reduction saved)'.format(end_mem, 100 * (start_mem - end_mem) / start_mem))
+    return df
--- 特征工程、建模和结果/source/pd_merger.py
+++
@@ -1,8 +0,0 @@
-import pandas
-# 安全连接两个表
-def merge_by_concat(df1, df2, merge_on):
-    merged_gf = df1[merge_on]
-    merged_gf = merged_gf.merge(df2, on=merge_on, how='left')
-    new_columns = [col for col in list(merged_gf) if col not in merge_on]
-    df1 = pd.concat([df1, merged_gf[new_columns]], axis=1)
-    return df1
